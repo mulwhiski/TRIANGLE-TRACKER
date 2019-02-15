64e08891df8a81544c853c21a1fe94e4972a305b
mulwhiski <marshalldennis27@gmail.com>
2019-02-15 15:45:08 +0300

    add scripts.js file

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var triangleType=["equlateral", "isoceles", "scalenes"]
+var sideA=prompt("enter number")
+var sideB=Prompt("enter number")
+var sidec=prompt("enter number")
+if (sideA + sideB < sideC || sideA + sideC < sideB || sideB + sideC < sideA)
+{alert("NOT A TRIANGLE! PLEASE RE-INPUT")}
+}else if (sideA === sideB && sideA + sideC)
