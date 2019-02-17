48cf0e1250e8873e5122d6ebbf0ff2c587cc4015
mulwhiski <marshalldennis27@gmail.com>
2019-02-17 09:20:25 +0300

    add scripts.js changes

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,10 @@ var sideB=Prompt("enter number")
 var sidec=prompt("enter number")
 if (sideA + sideB < sideC || sideA + sideC < sideB || sideB + sideC < sideA)
 {alert("NOT A TRIANGLE! PLEASE RE-INPUT")}
-}else if (sideA === sideB && sideA + sideC)
+}else if(sideA===sideB && sideB == sideC){
+  alert("equilateral");
+}else if (sideA===sideB || sideA == sideC || sideB == sideC) {
+  alert("isoceles");
+}else {
+  alert("scalene");
+}    
